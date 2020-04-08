840ca0fb7d4ca257dedabb92be2a886a6aca8e36
Samim Islam <samimislam9641@gmail.com>
2020-04-08 14:47:05 +0530

    Create ptr_array.c

---
+++ POINTER/ptr_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int *ptr[5]; //ptr can store address of 5 integers
+int p=1, q=2, r=3, s=4, t=5;
+ptr[0]=&p;
+ptr[1]=&q;
+ptr[2]=&r;
+ptr[3]=&s;
+ptr[4]=&t;
+printf("\n%d", *ptr[3]);
+return 0;
+}
